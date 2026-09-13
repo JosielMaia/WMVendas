a061083989ec5c212793e8f9bb8b3a489a909cba
Josiel <76920964+JosielMaia@users.noreply.github.com>
2026-09-12 21:50:03 -0300

    Adiciona fidelidade de clientes em public/sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "wm-vendas-v5";
+const CACHE = "wm-vendas-v6";
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(["/", "/favicon.svg", "/manifest.webmanifest"])));
   self.skipWaiting();
