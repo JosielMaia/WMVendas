e1f275ad5f12aef7def2401e611c4596dc15d60c
Josiel <76920964+JosielMaia@users.noreply.github.com>
2026-09-13 12:35:44 -0300

    chore: atualiza cache do PWA para correção móvel

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "wm-vendas-v7";
+const CACHE = "wm-vendas-v8";
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(["/", "/favicon.svg", "/manifest.webmanifest"])));
   self.skipWaiting();
