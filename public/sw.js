a0b2e6945348a1ad9b6e4f0438e04428ef96366b
Josiel <76920964+JosielMaia@users.noreply.github.com>
2026-09-13 13:22:41 -0300

    chore: atualiza PWA para reserva com entrada

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "wm-vendas-v11";
+const CACHE = "wm-vendas-v12";
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(["/", "/favicon.svg", "/manifest.webmanifest"])));
   self.skipWaiting();
