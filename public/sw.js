63878fa0769e6c6cd57e1eeb2f574b8b33bfd4a0
Josiel <76920964+JosielMaia@users.noreply.github.com>
2026-09-14 07:37:14 -0300

    chore: atualiza PWA para navegação da vendedora

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "wm-vendas-v14";
+const CACHE = "wm-vendas-v15";
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(["/", "/favicon.svg", "/manifest.webmanifest"])));
   self.skipWaiting();
