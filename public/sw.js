ee97c155e7c6e7c8fbdaa8a01772fbb6ed4598b6
Josiel <76920964+JosielMaia@users.noreply.github.com>
2026-09-14 08:58:24 -0300

    chore: atualizar PWA para venda expressa

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "wm-vendas-v18";
+const CACHE = "wm-vendas-v19";
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(["/", "/favicon.svg", "/manifest.webmanifest"])));
   self.skipWaiting();
