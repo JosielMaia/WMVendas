ddaf1a48f0c9dfcadd8c3d765286d42bc25ba8bc
Josiel <76920964+JosielMaia@users.noreply.github.com>
2026-09-14 07:27:36 -0300

    chore: atualiza PWA com reconexão da loja

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "wm-vendas-v13";
+const CACHE = "wm-vendas-v14";
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(["/", "/favicon.svg", "/manifest.webmanifest"])));
   self.skipWaiting();
