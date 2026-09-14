1080952556e8263985ed877dcc71ef51cbaeeea5
Josiel <76920964+JosielMaia@users.noreply.github.com>
2026-09-14 12:46:47 -0300

    chore: atualizar PWA após catálogo e desempenho

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "wm-vendas-v20";
+const CACHE = "wm-vendas-v21";
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(["/", "/favicon.svg", "/manifest.webmanifest"])));
   self.skipWaiting();
