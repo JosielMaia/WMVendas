03ac9c04dc0c3ab49d5422486abf0b1179d92422
Josiel <76920964+JosielMaia@users.noreply.github.com>
2026-09-14 07:19:22 -0300

    chore: atualiza PWA para fluxo público seguro

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "wm-vendas-v12";
+const CACHE = "wm-vendas-v13";
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(["/", "/favicon.svg", "/manifest.webmanifest"])));
   self.skipWaiting();
