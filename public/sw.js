a5f399a922a573c6fe038a97051eaf35e9eb7b3a
Josiel <76920964+JosielMaia@users.noreply.github.com>
2026-09-14 09:43:21 -0300

    chore: atualizar PWA para modo offline

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "wm-vendas-v19";
+const CACHE = "wm-vendas-v20";
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(["/", "/favicon.svg", "/manifest.webmanifest"])));
   self.skipWaiting();
